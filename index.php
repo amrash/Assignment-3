dde656dd2f920b0e979c3f5066767a0f06c5d97d
Amreen <amreenashraf@gmail.com>
2019-06-28 00:21:23 -0400

    This is commit 2

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
-hello world
+<!doctype html>
+<html lang="en">
+  <head>
+  <? include "head.php"?>
+  </head>
+  <body>
+  
+  <!-- header goes here -->
+  
+<div class="container-my-5">
+
+<div class="row-header">
+  
+    <div class="col-12 text-center pt-4">
+
+<h1>HH</h1>
+
+    </div>
+
+</div>
+
+</div>
+   
+  <!-- svg animation goes here -->
+  
+  <div class="container-fluid">
+
+ <? include "svg.php" ?>
+
+
+
+ </div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <footer class="page-footer font-small black">
+        <!-- //<div class="footer-copyright text-center mt-9 py-3"> -->
+        <div class="fixed-bottom">
+           <p>© 2019 Designed by Amreen Ashraf</p>
+            <!-- </div> -->
+            </div>
+    </footer>
+
+
+  </body>
+</html>
---
+++ svg.php
@@ -0,0 +1,10 @@
+<svg>
+
+  
+<circle cx="100" cy="100" r="100" stroke="black" stroke-width="3" fill="red" />
+
+
+
+
+
+</svg>
